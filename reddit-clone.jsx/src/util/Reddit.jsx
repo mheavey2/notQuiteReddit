36930aabafedc47c133bb81b6ace248dfcebc6b6
mheavey2 <mheavey2@gmail.com>
2024-04-20 19:21:05 +0100

    add function to fetch username data

--- reddit-clone.jsx/src/util/Reddit.jsx
+++ reddit-clone.jsx/src/util/Reddit.jsx
@@ -33,11 +33,35 @@ const Reddit = {
   },
 
   // step 2: get username data from Reddit
+  getUsername() {
+    if (!accessToken) {
+      return Promise.reject(new Error("Access token is missing"));
+    }
+    const userEndpoint = "";
+    return fetch(userEndpoint, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    })
+      .then((response) => {
+        if (response.status === 200) {
+          return response.json();
+        } else {
+          throw new Error("Failed to fetch user data");
+        }
+      })
+      .then((data) => {
+        const userName = data.display_name; //TODO:verify this is correct in api docs
+        console.log(`username is: ${userName}`);
+        userId = data.id; //TODO: also verify this
+      });
+  },
 
   // step 3: fetch the results for search input
   async searchItem(searchInput) {
     // top 10 results,sorted by relevance
-    const searchEndpoint = `${searchBaseURL}${searchInput}&sort=relevance&limit=1`;
+    const searchEndpoint = `${searchBaseURL}${searchInput}&sort=relevance&limit=10`;
 
     return fetch(searchEndpoint, {
       method: "GET",
